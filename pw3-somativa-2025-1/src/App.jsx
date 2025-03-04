b5812f35833b33d5a1f9c453d51c44c014f83cc8
Fabiana <fabilopes1997@gmail.com>
2025-03-04 20:54:16 -0300

    criação de links da pagina

--- pw3-somativa-2025-1/src/App.jsx
+++ pw3-somativa-2025-1/src/App.jsx
@@ -1,18 +1,45 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import './App.css'
+
+/* IMPORTAÇÃO DO NAVBAR */
 import NavBar from './Components/layout/NavBar'
+import Container from './components/layout/Container'
+
+/* IMPORTS DOS COMPONENTES DE PÁGINAS */
+import Home from './components/pages/Home'
+import CreateBook from './components/pages/CreateBook'
+import ListBook from './components/pages/ListBook'
+
 
 function App() {
- 
 
   return (
     <>
+    
       <div>
-       
-<NavBar/>
-      
+
+        <BrowserRouter>
+
+          <Container>
+
+            <Routes>
+
+              <Route path='/' element={<NavBar />}>
+
+                <Route path='/' element={<Home />} />
+                <Route path='/newBook' element={<CreateBook />} />
+                <Route path='/listBook' element={<ListBook />} />
+
+              </Route>
+
+            </Routes>
+
+          </Container>
+
+        </BrowserRouter>
+
       </div>
-      
     </>
   )
 }
--- pw3-somativa-2025-1/src/Components/BookCard.jsx
+++ pw3-somativa-2025-1/src/Components/BookCard.jsx
@@ -2,15 +2,17 @@ import Button from './Button'
 
 import style from './BookCard.module.css'
 
-const BookCard = ({titulo, autor, imagem})=>{ 
-    return (
-    <div className={style.bookCard}>                    
-        <h3 className={style.titulo}>{titulo}</h3>
-        <p className={style.autor}>{autor}</p>
-        <img src={imagem} alt='Capa: Cavernas de aço'/>
-        <Button label='DETALHE' />
-    </div>
+const BookCard = ({titulo, autor, imagem})=>{
+    return(
+        
+        <div className={style.bookCard}>
+            <h3 className={style.titulo}>{titulo}</h3>
+            <p className={style.autor}>{autor}</p>
+            <img src={imagem} alt="Capa: As Cavernas de Aço" />
+            <Button label='DETALHE' />
+        </div>
+        
     )
 }
 
-export default BookCard
+export default BookCard
